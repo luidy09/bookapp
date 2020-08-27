c78cc97665385684e1efca89ec1d6b57406252d0
EdroViegas <pedropombalviegas@gmail.com>
2020-08-27 15:28:43 +0100

    API requests

---
+++ lib/app/network_requests/api_service.dart
@@ -0,0 +1,10 @@
+class ApiService {
+  final apiUrl = "";
+  final String _apiKey = "";
+  final String _query = "";
+  final String _limit = "";
+
+  String get apiKey => _apiKey;
+  String get query => _query;
+  String get limit => _limit;
+}
